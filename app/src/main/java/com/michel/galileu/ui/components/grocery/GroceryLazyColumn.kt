5ead9a772d7781da9057266228f85dbf6504c152
Michel Silva <michelalmeidasilva@outlook.pt>
2023-02-25 15:38:46 -0300

    wip: added a new routes.

---
+++ app/src/main/java/com/michel/galileu/ui/components/grocery/GroceryLazyColumn.kt
@@ -0,0 +1,72 @@
+package com.michel.galileu.ui.components.grocery
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.michel.galileu.ui.screens.grocery.ItemParam
+
+
+@Composable
+fun ListGroceryItems(
+    onUpdateList: () -> Unit,
+    data: List<ItemParam>,
+    onClickItem: (value: Any) -> Unit,
+    onChangeSearchText: () -> Unit,
+    searchTextValue: String
+) {
+    Column(modifier = Modifier.padding(bottom = 60.dp, start = 16.dp, end = 16.dp)) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(all = 4.dp)
+        ) {
+            itemsIndexed(items = data) { index, item ->
+                ItemCard(
+                    item = item,
+                    onClick = { onClickItem(item.name) }
+                )
+            }
+        }
+
+    }
+}
+
+
+@Composable
+fun ItemCard(item: ItemParam, onClick: () -> Unit) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+
+    ) {
+        Text(item.name)
+        Spacer(Modifier.width(8.dp))
+        Text(item.category)
+        Spacer(Modifier.width(8.dp))
+        Text(item.priceAmount)
+        IconButton(onClick = onClick) {
+            Icon(
+                modifier = Modifier.size(24.dp),
+                imageVector = Icons.Rounded.KeyboardArrowRight,
+                contentDescription = "on click at list item",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
